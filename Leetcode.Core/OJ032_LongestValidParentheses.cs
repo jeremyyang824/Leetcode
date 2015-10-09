7154f6bfcb8402d67609eb3bbb0835a913342d3f
jeremyyang824 <jeremyyang824@aliyun.com>
2015-10-09 16:22:13 +0800

    20151009

---
+++ Leetcode.Core/OJ032_LongestValidParentheses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Core
+{
+    /*
+     * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+     * For "(()", the longest valid parentheses substring is "()", which has length = 2.
+     * Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+     */
+    public class OJ032_LongestValidParentheses
+    {
+        public int LongestValidParentheses(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            char[] arr = s.ToCharArray();
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    if (stack.Count != 0 && arr[stack.Peek()] == '(')
+                    {
+                        stack.Pop();
+                        result = Math.Max((stack.Count == 0 ? i + 1 : i - stack.Peek()), result);
+                    }
+                    else
+                    {
+                        stack.Push(i);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Leetcode.Test/OJ032_LongestValidParenthesesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Leetcode.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Test
+{
+    [TestClass]
+    public class OJ032_LongestValidParenthesesTest
+    {
+        [TestMethod]
+        public void OJ032_LongestValidParenthesesTest1()
+        {
+            int result = new OJ032_LongestValidParentheses().LongestValidParentheses("(()");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void OJ032_LongestValidParenthesesTest2()
+        {
+            int result = new OJ032_LongestValidParentheses().LongestValidParentheses(")()())");
+            Assert.AreEqual(4, result);
+        }
+    }
+}
