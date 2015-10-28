f638a187aad79122f295ee363886a12660bff25a
jeremyyang824 <jeremyyang824@aliyun.com>
2015-10-28 16:09:54 +0800

    20151028

---
+++ Leetcode.Core/OJ050_Pow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Core
+{
+    /*
+     * Implement pow(x, n).
+     */
+    public class OJ050_Pow
+    {
+        public double MyPow(double x, int n)
+        {
+            if (n < 0)
+                return 1d / pow(x, n * -1);
+            else
+                return pow(x, n);
+        }
+
+        private double pow(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+
+            double temp = this.pow(x, n / 2);
+
+            if (n % 2 == 0)
+                return temp * temp;
+            else
+                return temp * temp * x;
+        }
+    }
+}
---
+++ Leetcode.Test/OJ050_PowTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Leetcode.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Test
+{
+    [TestClass]
+    public class OJ050_PowTest
+    {
+        [TestMethod]
+        public void OJ050_PowTest1()
+        {
+            double result = new OJ050_Pow().MyPow(3, 0);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void OJ050_PowTest2()
+        {
+            double result = new OJ050_Pow().MyPow(3, 3);
+            Assert.AreEqual(27, result);
+        }
+
+        [TestMethod]
+        public void OJ050_PowTest3()
+        {
+            double result = new OJ050_Pow().MyPow(2, -2);
+            Assert.AreEqual(0.25, result);
+        }
+
+        [TestMethod]
+        public void OJ050_PowTest4()
+        {
+            double result = new OJ050_Pow().MyPow(2, -1);
+            Assert.AreEqual(0.5, result);
+        }
+    }
+}
