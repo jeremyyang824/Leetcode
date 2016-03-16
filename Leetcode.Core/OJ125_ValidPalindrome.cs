7f8e54b8ee545c1a64bce464ae7bb598822f2598
jeremyyang824 <jeremyyang824@aliyun.com>
2016-03-16 10:00:44 +0800

    20160316

---
+++ Leetcode.Core/OJ125_ValidPalindrome.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.Core
+{
+    /*
+     * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+     * For example,
+     * "A man, a plan, a canal: Panama" is a palindrome.
+     * "race a car" is not a palindrome.
+     * 
+     * Note:
+     * Have you consider that the string might be empty? This is a good question to ask during an interview.
+     * For the purpose of this problem, we define empty string as valid palindrome.
+     */
+    public class OJ125_ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (s == null)
+                return false;
+            if (s == string.Empty)
+                return true;
+
+            char[] charArr = s.ToLower().ToCharArray();
+            int l = 0, r = s.Length - 1;
+
+            while (l < r)
+            {
+                char cl = charArr[l];
+                while ((cl < 'a' || cl > 'z') && (cl < '0' || cl > '9'))
+                {
+                    if (++l >= r)
+                        return true;
+                    cl = charArr[l];
+                }
+
+                char cr = charArr[r];
+                while ((cr < 'a' || cr > 'z') && (cr < '0' || cr > '9'))
+                {
+                    if (--r <= l)
+                        return true;
+                    cr = charArr[r];
+                }
+
+                if (cl != cr)
+                    return false;
+
+                l++;
+                r--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Leetcode.Test/OJ125_ValidPalindromeTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Leetcode.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Test
+{
+    [TestClass]
+    public class OJ125_ValidPalindromeTest
+    {
+        [TestMethod]
+        public void OJ125_ValidPalindromeTest1()
+        {
+            bool result = new OJ125_ValidPalindrome().IsPalindrome("A man, a plan, a canal: Panama");
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void OJ125_ValidPalindromeTest2()
+        {
+            bool result = new OJ125_ValidPalindrome().IsPalindrome("race a car");
+            Assert.AreEqual(false, result);
+        }
+    }
+}
