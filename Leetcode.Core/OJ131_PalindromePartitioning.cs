4a59fd3099d2f34341057470183f2519b36a9bf7
jeremyyang824 <jeremyyang824@aliyun.com>
2016-05-12 12:20:43 +0800

    20160512

---
+++ Leetcode.Core/OJ131_PalindromePartitioning.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Core
+{
+    public class OJ131_PalindromePartitioning
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            IList<IList<string>> result = new List<IList<string>>();
+            IList<string> list = new List<string>();
+            if (s == null || s.Length < 1)
+                return result;
+
+            this.dfs(result, list, s);
+            return result;
+        }
+
+        private void dfs(IList<IList<string>> result, IList<string> list, string s)
+        {
+            //当传入的字符串长度等于0，则这个集合分割list满足条件
+            int len = s.Length;
+            if (len < 1)
+                result.Add(new List<string>(list)); //复制list并保存
+
+            for (int i = 1; i <= len; i++)
+            {
+                string leftStr = s.Substring(0, i);
+                if (this.isPalindrome(leftStr))
+                {
+                    list.Add(leftStr);
+                    string rightStr = s.Substring(i);
+                    dfs(result, list, rightStr);
+                    list.RemoveAt(list.Count - 1);  //该拆分失败，回溯上一轮
+                }
+            }
+        }
+
+        private bool isPalindrome(string s)
+        {
+            int i = 0,
+                j = s.Length - 1;
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                    return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Leetcode.Test/OJ131_PalindromePartitioningTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Leetcode.Core;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Test
+{
+    [TestClass]
+    public class OJ131_PalindromePartitioningTest
+    {
+        [TestMethod]
+        public void OJ131_PalindromePartitioningTest1()
+        {
+            IList<IList<string>> result = new OJ131_PalindromePartitioning().Partition("aab");
+            ArrayAssert.AreEqual(new string[] { "aa", "b" }, result[1].ToArray());
+            ArrayAssert.AreEqual(new string[] { "a", "a", "b" }, result[0].ToArray());
+        }
+    }
+}
